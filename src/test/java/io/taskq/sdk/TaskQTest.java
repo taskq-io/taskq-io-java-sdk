391c719b5f724f53d6679a805965ae4fde4adb67
Łukasz Kuśnierz <gitlab@lukaszkusnierz.org>
2016-10-19 20:29:28 +0200

    fix tests and compile for 1.6

--- src/test/java/io/taskq/sdk/TaskQTest.java
+++ src/test/java/io/taskq/sdk/TaskQTest.java
@@ -21,7 +21,7 @@ public class TaskQTest {
 
 	@Test(expected = TaskQException.class)
 	public void unauthorized() {
-		final TaskQ taskQ = new TaskQ().setApiKey("lows6ysX7D482sAb87p0j5oYsvdFBroDtSOlN5tEbaL1Dv7X4gz0G25B0XG7Xcrh");
+		final TaskQ taskQ = new TaskQ().setApiKey("invalid");
 		taskQ.useUrl("/tasks/unit-tests")
 				.withParam("sample_param", "sample value")
 				.withParam("param2", "lol")
